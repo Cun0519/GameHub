647cbd65a3e5d136c9f9f2c1a594dbe376d9ad65
cunxie <544842679@qq.com>
2020-08-05 03:14:30 +0800

    完成AvalonPlayActivity；初步完成Avalon

--- app/src/main/java/com/cunxie/gamehub/Activity/AvalonAssignmentActivity.java
+++ app/src/main/java/com/cunxie/gamehub/Activity/AvalonAssignmentActivity.java
@@ -8,17 +8,25 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.ViewAnimationUtils;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 import com.cunxie.gamehub.AvalonRole;
 import com.cunxie.gamehub.R;
 import com.cunxie.gamehub.Util.Signal;
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
 public class AvalonAssignmentActivity extends AppCompatActivity implements View.OnClickListener{
 
+    //count为全部玩家的总数
     private int count;
+    //number为当前玩家的序号
+    private int number = 1;
+
+    private boolean checkFlag = false;
+
     private List<AvalonRole> avalonRoleList = new ArrayList<>();
     private List<Integer> avalonAssignmentList = new ArrayList<>();
     private TextView textView0;
@@ -27,8 +35,7 @@ public class AvalonAssignmentActivity extends AppCompatActivity implements View.
     private Button contentButton;
     private Button confirmButton;
     private Button playButton;
-    private int number = 1;
-    private boolean checkFlag = false;
+    private ImageView roleImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -64,12 +71,16 @@ public class AvalonAssignmentActivity extends AppCompatActivity implements View.
         textView0.setText("一共" + count + "位玩家");
         textView1 = (TextView) findViewById(R.id.text_avalon_assignment_1);
         textView1.setText("请第" + number + "位玩家查看身份");
+
+        roleImage = (ImageView) findViewById(R.id.image_avalon_assignment_role);
     }
 
     @Override
     public void onClick(View view) {
+        AvalonRole role = avalonRoleList.get(avalonAssignmentList.get(number - 1));
         switch (view.getId()) {
             case R.id.button_avalon_assignment_check:
+                //关闭角色黑幕
                 Animator animCheck = ViewAnimationUtils.createCircularReveal(checkButton,
                         checkButton.getWidth() / 2, checkButton.getHeight() / 2, checkButton.getWidth(), 0);
                 animCheck.addListener(new AnimatorListenerAdapter() {
@@ -80,6 +91,10 @@ public class AvalonAssignmentActivity extends AppCompatActivity implements View.
                     }
                 });
                 animCheck.start();
+                //显示角色
+                roleImage.setImageResource(role.getImageId());
+                roleImage.setVisibility(View.VISIBLE);
+                //显示开始按键
                 if (number == count) {
                     Animator animPlay = ViewAnimationUtils.createCircularReveal(playButton,
                             playButton.getWidth() / 2, playButton.getHeight() / 2, 0, playButton.getWidth());
@@ -96,7 +111,6 @@ public class AvalonAssignmentActivity extends AppCompatActivity implements View.
                 break;
             case R.id.button_avalon_assignment_content:
                 if (checkFlag) {
-                    AvalonRole role = avalonRoleList.get(avalonAssignmentList.get(number - 1));
                     role.goToContent(AvalonAssignmentActivity.this);
                 } else {
                     Signal.signalToast(AvalonAssignmentActivity.this, "请查看身份");
@@ -108,6 +122,9 @@ public class AvalonAssignmentActivity extends AppCompatActivity implements View.
                     break;
                 }
                 if (checkFlag) {
+                    //隐藏角色
+                    roleImage.setVisibility(View.INVISIBLE);
+                    //开启角色黑幕
                     Animator animConfirm = ViewAnimationUtils.createCircularReveal(checkButton,
                             checkButton.getWidth() / 2, checkButton.getHeight() / 2, 0, checkButton.getWidth());
                     animConfirm.addListener(new AnimatorListenerAdapter() {
@@ -127,6 +144,11 @@ public class AvalonAssignmentActivity extends AppCompatActivity implements View.
                 break;
             case R.id.button_avalon_assignment_play:
                 Signal.signalToast(AvalonAssignmentActivity.this, "游戏开始");
+                Intent intent = new Intent(AvalonAssignmentActivity.this, AvalonPlayActivity.class);
+                intent.putExtra("avalonRoleList", (Serializable)avalonRoleList);
+                intent.putExtra("avalonAssignmentList", (Serializable)avalonAssignmentList);
+                startActivity(intent);
+                finish();
                 break;
         }
     }
---
+++ app/src/main/java/com/cunxie/gamehub/Activity/AvalonPlayActivity.java
@@ -0,0 +1,64 @@
+package com.cunxie.gamehub.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.NumberPicker;
+import com.cunxie.gamehub.AvalonRole;
+import com.cunxie.gamehub.R;
+import com.cunxie.gamehub.Util.Signal;
+import java.util.ArrayList;
+import java.util.List;
+
+public class AvalonPlayActivity extends AppCompatActivity {
+
+    private NumberPicker numberPicker;
+    private Button buttonCheck;
+    private Button buttonFinish;
+
+    private List<AvalonRole> avalonRoleList = new ArrayList<>();
+    private List<Integer> avalonAssignmentList = new ArrayList<>();
+
+    private int count;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_avalon_play);
+
+        Intent intent = getIntent();
+        avalonRoleList = (List<AvalonRole>) intent.getSerializableExtra("avalonRoleList");
+        avalonAssignmentList = (List<Integer>) intent.getSerializableExtra("avalonAssignmentList");
+        if (avalonRoleList.size() != avalonAssignmentList.size()) {
+            Signal.signalAlertDialog(AvalonPlayActivity.this, "Error", "Error List Size");
+        }
+
+        numberPicker = (NumberPicker) findViewById(R.id.numberpicker_avalon_play_number);
+        numberPicker.setMinValue(1);
+        numberPicker.setMaxValue(avalonRoleList.size());
+        numberPicker.setValue(1);
+        count = 1;
+        numberPicker.setOnValueChangedListener(new NumberPicker.OnValueChangeListener() {
+            @Override
+            public void onValueChange(NumberPicker numberPicker, int i, int i1) {
+                count = i1;
+            }
+        });
+        buttonCheck = (Button) findViewById(R.id.button_avalon_play_check);
+        buttonCheck.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                avalonRoleList.get(avalonAssignmentList.get(count - 1)).goToContent(AvalonPlayActivity.this);
+            }
+        });
+        buttonFinish = (Button) findViewById(R.id.button_avalon_play_finish);
+        buttonFinish.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+    }
+}
